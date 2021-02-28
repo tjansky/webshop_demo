90c02b2986a08b6b6160b197681de2e71fbfccf0
tjansky <tomislav.jansky@gmail.com>
2021-02-28 22:26:09 +0100

    Sucessfuly rendered products from db on frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Compnet';
+  products: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/Products").subscribe((res: any) => {
+      this.products = res.data;
+      console.log(this.products);
+    }, error =>{
+      console.log(error);
+    });
+  }
 }
