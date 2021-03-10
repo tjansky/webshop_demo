88d10fa39c7f8f5cb7578657a171672418cb2ff2
tjansky <tomislav.jansky@fina.hr>
2021-03-10 09:52:55 +0100

    Implemented pagination

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -14,9 +15,8 @@ export class ShopComponent implements OnInit {
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
-  brandIdSelected = 0;
-  typeIdSelected = 0;
-  sortSelected = "name";
+  shopParams = new ShopParams();
+  totalCount: number;
   sortOptions = [
     {name: "Alphabetic", value: "name"},
     {name: "Price: Low to High", value: "priceAsc"},
@@ -32,8 +32,11 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected).subscribe(res => {
+    this.shopService.getProducts(this.shopParams).subscribe(res => {
       this.products = res.data;
+      this.shopParams.pageNumber = res.pageIndex;
+      this.shopParams.pageSize = res.pageSize;
+      this.totalCount = res.count;
     }, error => {
       console.log(error);
     });
@@ -56,17 +59,22 @@ export class ShopComponent implements OnInit {
   }
 
   onBrandSelected(brandId: number) {
-    this.brandIdSelected = brandId;
+    this.shopParams.brandId = brandId;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
-    this.typeIdSelected = typeId;
+    this.shopParams.typeId = typeId;
     this.getProducts();
   }
 
   onSortSelected(sort: string) {
-    this.sortSelected = sort;
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any) {
+    this.shopParams.pageNumber = event.page;
     this.getProducts();
   }
 
