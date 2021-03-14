d69af1e3165243669395d8c9968b2ea2cba56a57
tjansky <tomislav.jansky@gmail.com>
2021-03-14 20:36:14 +0100

    Improved the internal serverr error component

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
@@ -36,7 +36,8 @@ export class ErrorInterceptor implements HttpInterceptor {
           }
           
           if (error.status === 500) {
-            this.router.navigateByUrl('/server-error');
+            const navigationExtras: NavigationExtras = {state: {error: error.error}};
+            this.router.navigateByUrl('/server-error', navigationExtras);
           }
         }
         return throwError(error);
