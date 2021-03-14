672e1808e517c2272593141b7bbf962b2db9a65b
tjansky <tomislav.jansky@gmail.com>
2021-03-14 11:49:08 +0100

    Created component for testing errors, 500 and 404 errors handled

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42').subscribe(res => {
+      console.log(res);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(res => {
+      console.log(res);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(res => {
+      console.log(res);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'products/fortytwo').subscribe(res => {
+      console.log(res);
+    }, error => {
+      console.log(error);
+    });
+  }
+  
+  
+
+}
