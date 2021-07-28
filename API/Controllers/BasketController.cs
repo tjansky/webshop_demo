3f1a5bd23a3027b0f2cc1ed8e89fc7ff839d1c92
tjansky <tomislav.jansky@fina.hr>
2021-07-28 14:42:10 +0200

    Basket controller added

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+
+            // if basket is null, create new basket with id client created
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+
+            return Ok(updatedBasket);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<bool>> DeleteBasket(string id)
+        {
+            return await _basketRepository.DeleteBasketAsync(id);
+        }
+
+    }
+}
