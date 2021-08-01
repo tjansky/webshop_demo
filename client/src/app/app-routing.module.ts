43ce0457b04e32402f18e41a90004bb872ea413e
tjansky <tomislav.jansky@gmail.com>
2021-08-01 12:50:37 +0200

    Added basket module, component, service and routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
   {path: 'test-error', component: TestErrorComponent},
   {path: 'server-error', component: ServerErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
