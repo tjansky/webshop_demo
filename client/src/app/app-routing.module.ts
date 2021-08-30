8c37b8a7832a7fcde785095693ed5a1384a44571
tjansky <tomislav.jansky@fina.hr>
2021-08-30 15:51:41 +0200

    Added checkout module, routing module, service and main component templates

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: {breadcrumb: 'checkout'}},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
