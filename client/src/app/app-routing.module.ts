7f0878ae7d8fc74f6432aa77f8efe60d63073cf8
tjansky <tomislav.jansky@gmail.com>
2021-09-08 19:24:00 +0200

    Added account module and address & user models

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
   {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: {breadcrumb: 'checkout'}},
+  {path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule), data: {breadcrumb: 'account'}},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
