10c3069c591f1dbdf22992cd2e1192fe543af391
tjansky <tomislav.jansky@gmail.com>
2021-09-08 18:20:32 +0200

    Updated swwager so it can use JWT token for auth

--- API/Extensions/SwaggerServiceExtension.cs
+++ API/Extensions/SwaggerServiceExtension.cs
@@ -10,6 +10,24 @@ namespace API.Extensions
         {
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "SkiNet API", Version = "v1"});
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securityScheme);
+                var securityRequirements = new OpenApiSecurityRequirement {{securityScheme, new[] {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirements);
             });
 
             return services;
