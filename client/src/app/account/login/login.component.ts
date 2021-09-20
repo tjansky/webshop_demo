cf459d1d8f75800197b13d32c3242b7507b81737
tjansky <tomislav.jansky@fina.hr>
2021-09-20 14:46:44 +0200

    Form validation made with reusable text input

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -18,7 +18,7 @@ export class LoginComponent implements OnInit {
 
   createLoginForm() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required)
     });
   }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,11 +5,12 @@ import { PagingHeaderComponent } from './components/paging-header/paging-header.
 import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent, TextInputComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -20,7 +21,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     PagingHeaderComponent,
     PagerComponent,
     OrderTotalsComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
