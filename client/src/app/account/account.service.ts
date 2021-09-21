b2f54ede05b02192a6e93d7da4bd3af8ca8ab306
tjansky <tomislav.jansky@fina.hr>
2021-09-21 09:36:21 +0200

    Added registration form

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -63,7 +63,7 @@ export class AccountService {
   }
 
   checkEmailExists(email: string) {
-    return this.http.get(this.baseUrl + '/account/emailexists?email=' + email);
+    return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
   }
 
 }
--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AsyncValidatorFn, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { of, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { AccountService } from '../account.service';
 
 @Component({
@@ -21,7 +23,7 @@ export class RegisterComponent implements OnInit {
   createRegisterForm() {
     this.registerForm = this.fb.group({
       displayName: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.email]],
+      email: [null, [Validators.required, Validators.email], [this.validateEmailNotTaken()]],
       password: [null, Validators.required]
     });
   }
@@ -33,6 +35,23 @@ export class RegisterComponent implements OnInit {
       console.log(error);
       this.errors = error.errors;
     });
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return control => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if (!control.value) {
+            return of(null);
+          }
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(res => {
+              return res ? {emailExists: true} : null
+            })
+          );
+        })
+      );
+    }
   }
 
 }
