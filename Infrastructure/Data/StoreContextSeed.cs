6f45c498075620b688e87fbf24d065af2f538efc
tjansky <tomislav.jansky@gmail.com>
2021-10-04 14:59:11 +0200

    Added configuration files for order entities and made migration

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -52,6 +53,20 @@ namespace Infrastructure.Data
                     foreach (var product in products)
                     {
                         context.Products.Add(product);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.DeliveryMethods.Any())
+                {
+                    var dmData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+
+                    var methods  = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
+                    
+                    foreach (var item in methods)
+                    {
+                        context.DeliveryMethods.Add(item);
                     }
 
                     await context.SaveChangesAsync();
