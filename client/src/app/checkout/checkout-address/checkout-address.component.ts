7ac1df9720dbbd7de5654533280e044191db8ac0
tjansky <tomislav.jansky@gmail.com>
2021-10-11 21:05:35 +0200

    Huge commit, stepper checkout form implemented

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-checkout-address',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-address.component.scss']
 })
 export class CheckoutAddressComponent implements OnInit {
+  @Input() checkoutForm: FormGroup
 
-  constructor() { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  saveUserAddress() {
+    this.accountService.updateUserAddress(this.checkoutForm.get('addressForm').value).subscribe(() => {
+      this.toastr.success('Address saved');
+    }, error => {
+      this.toastr.error(error.message);
+      console.log(error);
+    });
   }
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -8,16 +8,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { StepperComponent } from './components/stepper/stepper.component';
+import { BasketSummaryComponent } from './components/basket-summary/basket-summary.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent, TextInputComponent, StepperComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent, TextInputComponent, StepperComponent, BasketSummaryComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
     ReactiveFormsModule,
-    CdkStepperModule
+    CdkStepperModule,
+    RouterModule
   ],
   exports: [
     PaginationModule,
@@ -27,7 +30,8 @@ import { StepperComponent } from './components/stepper/stepper.component';
     ReactiveFormsModule,
     TextInputComponent,
     CdkStepperModule,
-    StepperComponent
+    StepperComponent,
+    BasketSummaryComponent
   ]
 })
 export class SharedModule { }
